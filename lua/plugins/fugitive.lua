09cd575d9eb9abe4e4d6f6356bafe376cfa2489b
Jan Henrik Weinstock <jan@mwa.re>
2026-01-13 11:18:32 +0100

    feat(fugitive): add keymap for git commit

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,5 +4,6 @@ return {
     vim.keymap.set('n', '<leader>gs', ':Git status<CR>', {})
     vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', {})
     vim.keymap.set('n', '<leader>gt', ':Git log --graph --oneline --all<CR>', {})
+    vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', {})
   end
 }
