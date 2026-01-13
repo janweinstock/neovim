7a7469ba83b25bb4228dc39a3eee95c9ce8b6a5c
Jan Henrik Weinstock <jan@mwa.re>
2026-01-13 15:35:05 +0100

    fix syntax highlighting for cpp

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,35 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  tag = "v0.9.3",
   build = ":TSUpdate",
   lazy = false,
-  config = function()
-    local config = require("nvim-treesitter.config")
-    config.setup({
-      ensure_installed = {
-        "bash",
-        "c",
-        "cmake",
-        "cpp",
-        "html",
-        "javascript",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "rust",
-        "yaml",
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
+
+  opts = {
+    ensure_installed = { "bash",
+      "bash",
+      "c",
+      "cmake",
+      "cpp",
+      "html",
+      "javascript",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "rust",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { "ruby" },
+    },
+    indent = { enable = true, disable = { "ruby" } },
+  },
+
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
   end,
 }
