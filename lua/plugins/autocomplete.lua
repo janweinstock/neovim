fe3aec6cb8080fa6f5096c904a552e711eabdfdb
Jan Henrik Weinstock <jan@mwa.re>
2026-01-16 13:24:28 +0100

    feat(autocomplete): add LuaSnip integration

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -4,12 +4,22 @@ return {
     dependencies = {
         { "hrsh7th/cmp-nvim-lsp" },
         { "hrsh7th/cmp-path" },
+        { "L3MON4D3/LuaSnip" },
+        { "saadparwaiz1/cmp_luasnip" },
     },
 
     config = function()
         local cmp = require("cmp")
+        local luasnip = require("luasnip")
+
+        luasnip.config.setup({})
 
         cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
             completion = { completeopt = "menu,menuone,noinsert" },
 
             mapping = cmp.mapping.preset.insert({
@@ -19,10 +29,21 @@ return {
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete({}),
+                ["<C-l>"] = cmp.mapping(function()
+                    if luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    end
+                end, { "i", "s" }),
+                ["<C-h>"] = cmp.mapping(function()
+                    if luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    end
+                end, { "i", "s" }),
             }),
 
             sources = {
                 { name = "nvim_lsp" },
+                { name = "luasnip" },
                 { name = "path" },
                 { name = "codecompanion" },
             },
