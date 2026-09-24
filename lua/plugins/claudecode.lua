861b495485249d82a128bd32c7e661ece5fefd49
Jan Henrik Weinstock <jan@mwa.re>
2026-09-24 09:10:21 +0200

    feat: adding claudecode plugin

---
+++ lua/plugins/claudecode.lua
@@ -0,0 +1,11 @@
+return {
+  "coder/claudecode.nvim",
+  dependencies = { "folke/snacks.nvim" },
+  config = true,
+  keys = {
+    { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+    { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send selection" },
+    { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+    { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+  },
+}
