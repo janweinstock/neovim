214af60323656e7aa751aed48e5ce11d3648b23a
Jan Henrik Weinstock <jan@mwa.re>
2026-09-25 08:22:48 +0200

    feat: limit lsp log size to 50mb

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,10 @@
+-- rotate lsp log when it exceeds 50MB, keeping one previous copy
+local lsp_log = vim.lsp.log.get_filename()
+local lsp_log_stat = vim.uv.fs_stat(lsp_log)
+if lsp_log_stat and lsp_log_stat.size > 50 * 1024 * 1024 then
+    os.rename(lsp_log, lsp_log .. ".old")
+end
+
 vim.lsp.enable({
     "lua_ls",
     "clangd",
